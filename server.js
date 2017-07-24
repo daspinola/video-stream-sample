ce8aafb1892fd1dcdac2603986d13d1663ec5a9c
Diogo Santos <diogo.alexs.santos@gmail.com>
2017-07-24 02:09:13 +0100

    Remove ; for consistency

--- server.js
+++ server.js
@@ -3,26 +3,27 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.htm'));
-});
+  res.sendFile(path.join(__dirname + '/index.htm'))
+})
 
 app.get('/video', function(req, res) {
-  const path = 'assets/sample.mp4';
-  const stat = fs.statSync(path);
-  const fileSize = stat.size;
-  const range = req.headers.range;
+  const path = 'assets/sample.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
 
   if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const partialStart = parts[0];
-    const partialEnd = parts[1];
-    const start = parseInt(partialStart, 10);
-    const end = partialEnd ? parseInt(partialEnd, 10) : fileSize-1;
-    const chunksize = (end-start)+1;
-    const file = fs.createReadStream(path, {start, end});
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
     const head = {
       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
       'Accept-Ranges': 'bytes',
@@ -30,17 +31,17 @@ app.get('/video', function(req, res) {
       'Content-Type': 'video/mp4',
     }
 
-    res.writeHead(206, head);
-    file.pipe(res);
+    res.writeHead(206, head)
+    file.pipe(res)
   } else {
     const head = {
       'Content-Length': fileSize,
       'Content-Type': 'video/mp4',
     }
-    res.writeHead(200, head);
-    fs.createReadStream(path).pipe(res);
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
   }
-});
+})
 
 app.listen(3000, function () {
   console.log('Listening on port 3000!')
